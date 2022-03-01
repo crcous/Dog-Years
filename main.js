13b4f2b6e4b2c271f89554158862d281713cc291
crcous <100642297+crcous@users.noreply.github.com>
2022-02-28 22:07:43 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+var myAge = '23';
+// This is my current age represented as trips around the sun.
+
+var earlyYears = '2';
+// This variable accounts for the beginning of my life.
+
+earlyYears *= 10.5;
+
+myAge -= 2;
+
+var laterYears = myAge;
+// This accounts for my years not including the early years.
+
+laterYears *= 4;
+// This is the representation of dog age compared to human years.
+
+console.log(earlyYears);
+
+console.log(laterYears);
+
+var myAgeInDogTears;
+myAgeInDogYears = earlyYears + laterYears;
+// This represents my total age in dog years.
+
+var myName = 'Chase';
+console.log(myName.toLowerCase());
+// Set my entire name to lowercase.
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
